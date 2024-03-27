5a78e4303fbd598b4d713a402e992b3be9f30c4b
Vlad <vladislavbymain@mail.ru>
2024-03-27 13:33:22 +0900

    Initial commit

---
+++ src/main/java/com/vlat/MedAppServer/MedAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.vlat.MedAppServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedAppServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vlat/MedAppServer/MedAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlat.MedAppServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
